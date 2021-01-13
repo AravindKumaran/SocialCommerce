5ca6a6ec1ec567fe99f0372c3683284bddd21287
Tamilvanan25 <tamilvanan@vinteksolution.com>
2021-01-13 22:40:19 +0530

    top nav

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Dimensions, Image, StyleSheet } from 'react-native';
+import { View, FlatList, Dimensions, Image, ImageOverlay, StyleSheet } from 'react-native';
 import Post from '../../components/Post';
 import { API, graphqlOperation } from 'aws-amplify';
-import Bgimage from '../assets/images/Bg_image.png';
-import Bgname from '../assets/images/Bg_name.png';
+// import Bgimage from '../../assets/images/Plus.png';
+// import Bgname from '../assets/images/Bg_name.png';
 
 import { listPosts } from '../../graphql/queries';
 
@@ -35,7 +35,8 @@ const Home = ({ navigation }) => {
   return (
     <View style={{  position: 'absolute', padding: 10, width: 400, height: 1500, top: 80, bottom: 50,
     paddingLeft: 4, paddingRight: 10, paddingBottom: 30, backgroundColor: '#292929' }}>
-      {/* <Image name={Bgimage} style={{ width: 75, height: 75}} /> */}
+      <Image source={require('../../assets/images/Logo.png')} size= {15} style={{ right: 0, left:0, height: 80, paddingLeft: 2, paddingRight: 2, width: 400, top: -80, position: 'absolute', borderRadius: 20}} />
+      {/* <Image source={require('../../assets/images/Background.png')}  style={{ top: -103, right: 10, paddingLeft: -50, position: 'absolute'}} /> */}
       {/* <Image name= {Bgimage}></Image> */}
       {/* <Image source={require('../assets/images/Bg_image')} style={{ width: 75, height: 75, borderRadius: 50, top: 20 }} /> */}
       {/* <Image source={require('../assets/images/Bg_name.png')} style={{ width: 75, height: 75, borderRadius: 50, top: 20 }} /> */}
