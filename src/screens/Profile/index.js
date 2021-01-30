809c24e334ab2faf63583ac5df7e477814c3bc67
Asfiya <asfiya@vinteksolution.com>
2021-01-30 15:00:58 +0530

    #SocialCommerce - google and fb Authentication

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -20,14 +20,14 @@ const ProfileScreen = () => {
 
   const [user, setUser] = useState(null);
 
-  function signin() {
-    Auth.federatedSignIn({provider: 'google'});
-  }
+  // function signin() {
+  //   Auth.federatedSignIn({provider: 'google'});
+  // }
 
-  // const signin = useCallback(() => {
-  //   Auth.federatedSignIn({ provider: 'google' });
-  //   setUser(true);
-  // }, []);
+  const signin = useCallback(() => {
+    Auth.federatedSignIn({ provider: 'google' });
+    setUser(true);
+  }, []);
 
   useEffect(() => {
     Auth.currentAuthenticatedUser()
@@ -47,14 +47,14 @@ const ProfileScreen = () => {
     <View style={styles.container}>
       {user === null ? (
         // <TouchableOpacity onPress={signin}>
-          // <View style={styles.button}>
-          <WebView style= {styles.button}
-            onPress={signin}
+        //   <View style={styles.button}>
+            <WebView style= {styles.button} 
+             onPress={signin}
             originWhitelist={['*']}
             source={{ uri: 'https://tiktok24dfe314-24dfe314-demo.auth.us-east-2.amazoncognito.com/login?redirect_uri=tiktok%3A%2F%2F&response_type=code&client_id=7dcbjoer98feb1f4spbn5p0g4l&identity_provider=google&scope=phone%20email%20openid%20profile%20aws.cognito.signin.user.admin&state=HcXprhpFinnP0yJWLg97AzKH0WvvD348&code_challenge=zyasMIpb4FzSb_x3T91xzwFKlQp_X5o3CV_L60nS1lM&code_challenge_method=S256&errorMessage=Login+option+is+not+available.+Please+try+another+one' }}       
-          />
-            // <Text style={styles.buttonText}>Sign in</Text>
-          // </View>
+          /> 
+        //     <Text style={styles.buttonText}>Sign in</Text>
+        //   </View>
         // </TouchableOpacity>
       ) : (
           <>
