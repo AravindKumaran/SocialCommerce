9d925262be9b5fc0a3d990394f5404aec583c5e5
avinashvintek <avinash@vinteksolution.com>
2021-03-11 11:47:38 +0530

    notification fixed

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -16,7 +16,11 @@ import Video from 'react-native-video';
 import DoubleClick from 'react-native-double-tap';
 import Comments from './comments';
 import PostLike from './postLike';
-import {updatePost} from '../../graphql/mutations';
+import {
+  updatePost,
+  createNotification,
+  createUserNotification,
+} from '../../graphql/mutations';
 import styles from './styles';
 import Feather from 'react-native-vector-icons/Feather';
 
