3c953a6664b3784534d47189cf1d1b260210572a
avinashvintek <avinash@vinteksolution.com>
2021-03-11 18:57:34 +0530

    other explore video play pause

--- src/screens/Search/trending.js
+++ src/screens/Search/trending.js
@@ -73,7 +73,15 @@ const videoUris = [
 ];
 
 const Trending = () => {
-  const [paused, setPaused] = useState(false);
+  const [paused, setPaused] = useState(true);
+  const [paused2, setPaused2] = useState(true);
+  const [paused3, setPaused3] = useState(true);
+  const [paused4, setPaused4] = useState(true);
+  const [paused5, setPaused5] = useState(true);
+  const [paused6, setPaused6] = useState(true);
+  const [paused7, setPaused7] = useState(true);
+  const [paused8, setPaused8] = useState(true);
+  const [paused9, setPaused9] = useState(true);
   const onPlayPausePress = () => {
     setPaused(!paused);
   };
@@ -111,195 +119,205 @@ const Trending = () => {
               style={StyleSheet.absoluteFill}
             />
           </View>
-          <View
-            style={{
-              // flex: 1,
-              width: 166,
-              height: 173,
-              top: 262,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 166,
-              height: 218,
-              top: 442,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
+          <TouchableWithoutFeedback onPress={() => setPaused2(!paused2)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 166,
+                height: 173,
+                top: 262,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused2}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused3(!paused3)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 166,
+                height: 218,
+                top: 442,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused3}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
         </View>
 
         <View style={{right: -5, top: 5}}>
-          <View
-            style={{
-              // flex: 1,
-              width: 103,
-              height: 108,
-              left: 171,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 103,
-              height: 108,
-              left: 280,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 103,
-              height: 108,
-              left: 171,
-              top: 115,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 103,
-              height: 108,
-              left: 280,
-              top: 115,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 215,
-              height: 257,
-              left: 171,
-              top: 230,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
-
-          <View
-            style={{
-              // flex: 1,
-              width: 215,
-              height: 166,
-              left: 171,
-              top: 495,
-              position: 'absolute',
-            }}>
-            <Video
-              muted={true}
-              paused={true}
-              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
-              posterResizeMode="cover"
-              source={{
-                uri: convertToProxyURL(
-                  'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                ),
-              }}
-              resizeMode="cover"
-              style={StyleSheet.absoluteFill}
-            />
-          </View>
+          <TouchableWithoutFeedback onPress={() => setPaused4(!paused4)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 103,
+                height: 108,
+                left: 171,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused4}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused5(!paused5)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 103,
+                height: 108,
+                left: 280,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused5}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused6(!paused6)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 103,
+                height: 108,
+                left: 171,
+                top: 115,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused6}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused7(!paused7)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 103,
+                height: 108,
+                left: 280,
+                top: 115,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused7}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused8(!paused8)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 215,
+                height: 257,
+                left: 171,
+                top: 230,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused8}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => setPaused9(!paused9)}>
+            <View
+              style={{
+                // flex: 1,
+                width: 215,
+                height: 166,
+                left: 171,
+                top: 495,
+                position: 'absolute',
+              }}>
+              <Video
+                muted={false}
+                paused={paused9}
+                poster="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/320px-Big_Buck_Bunny_thumbnail_vlc.png"
+                posterResizeMode="cover"
+                source={{
+                  uri: convertToProxyURL(
+                    'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                  ),
+                }}
+                resizeMode="cover"
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          </TouchableWithoutFeedback>
         </View>
 
         <View style={{top: 670}}>
