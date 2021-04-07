8efbd0ff2fefea28a851a0e94b278a133b5a4b40
avinashvintek <avinash@vinteksolution.com>
2021-04-07 17:18:31 +0530

    Brands

--- src/screens/Search/brands3.js
+++ src/screens/Search/brands3.js
@@ -1,116 +1,71 @@
-import React, {useState, useCallback} from 'react';
-import {StyleSheet, View, FlatList, Dimensions, Modal} from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Dimensions,
+  Modal,
+  Text,
+} from 'react-native';
+import {API, graphqlOperation, Auth} from 'aws-amplify';
+import {listPosts} from '../../graphql/queries';
+
 import TrendingVideo from './trendingVideo';
-import BrandFullScreenVideo from './fullScreenVideo';
+import FullScreenVideo from './fullScreenVideo';
 
 const vpHeight = Dimensions.get('window').height;
 const vpWidth = Dimensions.get('window').width;
 
-const uris = [
-  {
-    key: 1,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 2,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 3,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 4,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 5,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 6,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 7,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 8,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 9,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 10,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 11,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 12,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 13,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 14,
-    uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-  {
-    key: 15,
-    uri:
-      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-    height: 200,
-    width: '33.33%',
-  },
-];
-
 const Brands = () => {
   const [fullScreen, setFullScreen] = useState(false);
   const [curIdx, setCurIdx] = useState(0);
+  const [uris, setUris] = useState([]);
+  const [nextToken, setNextToken] = useState(undefined);
+  const [curLimit, setCurLimit] = useState(10);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await API.graphql(
+          graphqlOperation(listPosts, {
+            limit: curLimit,
+          }),
+        );
+        const allItems = response.data.listPosts.items;
+        const sortedItems = allItems.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+        );
+        console.log('sortedItems', response.data.listPosts.nextToken);
+        setNextToken(response.data.listPosts.nextToken);
+
+        setUris(sortedItems);
+      } catch (e) {
+        console.log('Caledd');
+        console.error(e);
+      }
+    };
+
+    fetchPost();
+  }, []);
+
+  const getMorePosts = async () => {
+    try {
+      if (nextToken) {
+        const response = await API.graphql(
+          graphqlOperation(listPosts, {
+            limit: curLimit + 15,
+            nextToken,
+          }),
+        );
+        console.log('AllItems', curLimit);
+        setCurLimit((lim) => lim + 15);
+        setNextToken(response.data.listPosts.nextToken);
+        setUris((post) => [...post, ...response.data.listPosts.items]);
+      }
+    } catch (error) {
+      console.log('Pagination Error', error);
+    }
+  };
 
   const handleFullScreen = useCallback(
     (idx, btn) => {
@@ -123,37 +78,30 @@ const Brands = () => {
     },
     [fullScreen],
   );
+
   const _renderItem = ({item, index}) => (
     <TrendingVideo
-      videoUri={item.uri}
+      videoUri={item.videoUri}
       idx={index}
       height={item.height}
       fullScreen={fullScreen}
       onFullScreen={handleFullScreen}
+      poster={item?.thumbnail}
+      item={item}
     />
   );
 
-  if (fullScreen) {
-    return (
-      <Modal>
-        <BrandFullScreenVideo
-          data={uris}
-          fullScreen={fullScreen}
-          handleFullScreen={handleFullScreen}
-          idx={curIdx}
-        />
-      </Modal>
-    );
-  }
-
   return (
     <View style={styles.container}>
+      <Text style={styles.text2}>Top Trending</Text>
       <FlatList
         nestedScrollEnabled={true}
         data={uris}
         numColumns={3}
         renderItem={_renderItem}
-        keyExtractor={(item) => item.key.toString()}
+        onEndReached={getMorePosts}
+        onEndReachedThreshold={0.5}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
   );
@@ -164,6 +112,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 5,
     marginBottom: 100,
+    marginTop: -500,
+  },
+  text2: {
+    marginVertical: 30,
+    color: '#FFFFFF',
+    fontWeight: '700',
+    textAlign: 'center',
+    fontFamily: 'Proxima Nova',
+    fontSize: 16,
   },
 });
 
