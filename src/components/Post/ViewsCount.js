f6c2d8fb2cfd79f3a9b1bfc76cbf52df27e51561
sundar-vintek <suntharamoorthy@vinteksolution.com>
2021-05-15 14:46:41 +0530

    view count added in home & search

---
+++ src/components/Post/ViewsCount.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, Image, Alert, ToastAndroid} from 'react-native';
+
+const ViewsCount = ({currentPost, OnIncView, currentIndex, currentVisibleIndex}) => {
+    const [views, setViews] = useState(currentPost.views === null ? 0:currentPost.views);
+
+    useEffect(async () => {
+        // console.log(currentIndex);
+        // console.log(currentVisibleIndex);
+
+        if (currentIndex === currentVisibleIndex) {
+            
+          const viewsCount = views + 1;          
+          OnIncView(viewsCount)
+          setViews(viewsCount);     
+        }
+    }, [currentVisibleIndex]);
+
+    return (
+        <View>
+            <Text>{views} Views</Text>
+        </View>
+    )
+}
+
+export default ViewsCount
