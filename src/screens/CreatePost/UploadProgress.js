9f6b9d8bf6d40752d764b7c72d8faa729008ddf1
sundar <sundar.ajay147@gmail.com>
2021-06-08 17:19:58 +0530

    hashtag dynamic view for 5 tags

--- src/screens/CreatePost/UploadProgress.js
+++ src/screens/CreatePost/UploadProgress.js
@@ -4,33 +4,80 @@ import styles from './styles';
 import * as Progress from 'react-native-progress';
 import {Context} from '../../context/Store';
 import {Storage, API, graphqlOperation, Auth} from 'aws-amplify';
-import {createPost} from '../../graphql/mutations';
+import {createPost, createHashTag, createPostHashTag} from '../../graphql/mutations';
+import {listHashTags} from '../../graphql/queries';
 
-function UploadProgress({uploadingPost, setPostRerender}) {
+function UploadProgress({uploadingPost, hashTag, setPostRerender}) {
     const [state, dispatch] = useContext(Context);
 
-    useEffect(async () => {    
-        //console.log('upload useeff', state.uploadStarted);    
-        if(state.uploadProgress == 100){
-            //console.log('uploadProgress',100);     
-            if(uploadingPost !== undefined){  
-                //console.log('uploadingPost',uploadingPost);              
-                const posRes = await API.graphql(
-                    graphqlOperation(createPost, {input: uploadingPost}),
-                );
-                console.log('posRes', posRes);
-                setPostRerender(true);
-                setPostRerender(false);
-            }      
-            dispatch({type: 'uploadStarted', payload: false})       
-            dispatch({type: 'uploadProgress', payload: 0})
-        }else if(state.uploadError != null){
-            dispatch({type: 'uploadStarted', payload: false})       
-            dispatch({type: 'uploadProgress', payload: 0})
-        }else if(state.uploadProgress > 0){
-            console.log('uploadProgress',state.uploadProgress);
-        }  
- 
+    useEffect(() => {  
+        const uploadVideoProgress = async () => {
+            //console.log('upload useeff', state.uploadStarted);    
+            if(state.uploadProgress == 100){
+                //console.log('uploadProgress',100);     
+                if(uploadingPost !== undefined){  
+                    //console.log('uploadingPost',uploadingPost); 
+                                
+                    const posRes = await API.graphql(
+                        graphqlOperation(createPost, {input: uploadingPost}),
+                    );
+                    console.log('posRes', posRes);
+
+                    if(hashTag){
+                        hashTag.map(async (h) => {
+                            const response = await API.graphql(
+                                graphqlOperation(listHashTags, {
+                                    filter: {
+                                        name: {eq: h}
+                                    }
+                                }),
+                            )
+
+                            console.log('response', response)
+
+                            if(!response.data.listHashTags.items){
+                                const hashTagRes = await API.graphql(
+                                    graphqlOperation(createHashTag, {input: {name: h}}),
+                                )
+                                const postHashTagRes = await API.graphql(
+                                    graphqlOperation(createPostHashTag, 
+                                        {
+                                            input: {
+                                                postID: posRes.data.createPost.id, 
+                                                hashTagID: hashTagRes.data.createHashTag.id
+                                            }
+                                        }
+                                    ),
+                                )                            
+                            }else{
+                                const postHashTagRes = await API.graphql(
+                                    graphqlOperation(createPostHashTag, 
+                                        {
+                                            input: {
+                                                postID: posRes.data.createPost.id, 
+                                                hashTagID: response.data.createHashTag.id
+                                            }
+                                        }
+                                    ),
+                                ) 
+                            }
+                            console.log('postHashTagRes', postHashTagRes)
+                        })
+                    }
+
+                    setPostRerender(true);
+                    setPostRerender(false);
+                }      
+                dispatch({type: 'uploadStarted', payload: false})       
+                dispatch({type: 'uploadProgress', payload: 0})
+            }else if(state.uploadError != null){
+                dispatch({type: 'uploadStarted', payload: false})       
+                dispatch({type: 'uploadProgress', payload: 0})
+            }else if(state.uploadProgress > 0){
+                console.log('uploadProgress',state.uploadProgress);
+            }  
+        }
+        uploadVideoProgress();
     }, [state.uploadProgress])
 
     return (       
